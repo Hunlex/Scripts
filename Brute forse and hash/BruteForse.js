3614614bb901ae327c3432d005500a6a656b94ca
Hast <43549217+hastyulia@users.noreply.github.com>
2018-11-28 00:18:34 +0500

    Create BruteForse.js

---
+++ Brute forse and hash/BruteForse.js
@@ -0,0 +1,26 @@
+var fso = new ActiveXObject('Scripting.FileSystemObject');
+var file = fso.OpenTextFile('string.txt');
+var string = file.ReadAll();
+file.Close();
+
+var substring = WSH.StdIn.ReadLine();
+
+var result = [];
+var start = (new Date()).getTime();
+
+for (var i = 0; i <= string.length - substring.length; i++) 
+{
+    for (var j = 0; string.charAt(i + j) == substring.charAt(j); j++) 
+    {
+        if (j == substring.length - 1) 
+        {
+            result.push(i);
+            break;
+        }
+    }
+}
+
+var end = (new Date()).getTime();
+if (result.length == 0)
+    WSH.stdOut.WriteLine('Substring not found');
+else WSH.stdOut.WriteLine('Brute Forse. Position: ' + result);
