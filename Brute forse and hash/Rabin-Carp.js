0265bf5fe328457f4c116ce294281cff824471d7
Hast <43549217+hastyulia@users.noreply.github.com>
2018-12-03 16:23:13 +0500

    Update Rabin-Carp.js

--- Brute forse and hash/Rabin-Carp.js
+++ Brute forse and hash/Rabin-Carp.js
@@ -6,20 +6,12 @@ function calculateHashRC(string)
     return hash;
 }
 
-var fso = new ActiveXObject('Scripting.FileSystemObject');
-var file = fso.OpenTextFile('string.txt');
-var string = file.ReadAll();
-file.Close();
-
-WSH.StdOut.Write('Enter substring: ')
-var substring = WSH.StdIn.ReadLine();
-
-WSH.echo('Rabin-Carp');
 var result = [];
 var substringHash = calculateHashRC(substring);
 var stringHash = calculateHashRC(string.substr(0, substring.length));
 var start = (new Date()).getTime();
 var collisionCount = 0;
+var fl = 0;
 
 for (var i = 0; i <= string.length - substring.length; i++) 
 {
@@ -30,21 +22,24 @@ for (var i = 0; i <= string.length - substring.length; i++)
             if (j == substring.length - 1) 
             {
                 result.push(i);
+                fl = 1;
                 break;
             }
-            
-            else 
-            {
-                collisionCount++;
-                break;
-            }
+        }
+        
+
+        if (fl == 1)
+        {
+            collisionCount++;
+            fl = 0;
         }
     }
 
     stringHash = 2 * (stringHash - string.charCodeAt(i) * (1 << substring.length)
-                                 + string.charCodeAt(i + substring.length));
+                            + string.charCodeAt(i + substring.length));
 }
 
+collisionCount -= result.length; 
 var end = (new Date()).getTime();
 if (result.length == 0)
     WSH.echo('Substring not found');
